80c84578fdbb94130625e6f87696cb88cd7e86a3
Katz <mykatz01@gmail.com>
2020-03-28 22:01:26 -0700

    really basic scaffolding

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  Row,
+  Col,
+  Container
+} from "reactstrap";
+
+import Navbar from "./components/navbar"
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Container fluid={true} className="container" style={{maxWidth: "100vw", flex: 1}}>
+        <Row className="height-full">
+          <Col xs="3" className="sidebar">Hello</Col>
+          <Col xs="auto" style={{flex: 1}}>
+
+              This zone is for the cards and stuff.
+
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavbarText
+} from 'reactstrap';
+
+import styled from 'styled-components';
+
+const Navtext = styled.span`
+    margin-right: 32px;
+    text-align: left;
+    letter-spacing: 0;
+    color: #FFFFFF;
+    opacity: 1;
+`;
+
+const MyNavbar = (props) => {
+
+  return (
+    <Navbar dark style={{display: "block"}}>
+        <Nav className="justify-content-between py-2">
+            <NavbarBrand className="mr-3">Github Dating</NavbarBrand>
+            <a href=""><NavbarText className="mr-2">Login</NavbarText></a>
+        </Nav>
+    </Navbar>
+  );
+}
+
+export default MyNavbar;
